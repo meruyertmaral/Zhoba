c57a1bd3f1417e588691d94e75e4b8b3eb6b3b58
Togzhan <togzhanbaktygozha@gmail.com>
2024-11-23 18:48:06 +0500

    Жаңа функционал қостым

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="kk">
+<head>
+    <meta charset="UTF-8">
+    <title>Кіру</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h2>Кіру</h2>
+    <form action="login_process.php" method="post">
+        <label>Email:</label>
+        <input type="email" name="email" required><br>
+        
+        <label>Құпия сөз:</label>
+        <input type="password" name="password" required><br>
+        
+        <button type="submit">Кіру</button>
+    </form>
+</body>
+</html>
